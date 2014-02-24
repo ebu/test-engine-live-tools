f06cf82ea8f1b7f18518c4a665a14c26c57dfbfb
Sjoerd Tieleman <sjoerd@sjoerdtieleman.nl>
2014-02-24 17:42:22 +0100

    Allow continuous operation

--- dasher/dasher.rb
+++ dasher/dasher.rb
@@ -1,11 +1,11 @@
 class Dasher
-  VARIANTS = ['out_hi', 'out_med', 'out_medlo', 'out_lo']
+  VARIANTS = ['out_med', 'out_medlo', 'out_lo', 'out_audio']
   INPUT_EXTENSION = "ts"
   SEGMENT_DIR = '/Users/tieleman/Documents/Code/EBU/video/output'
   OUTPUT_DIR = "#{SEGMENT_DIR}/segments"
   MP4BOX = 'MP4Box'
   
-  @last_seg = -1
+  @@last_seg = -1
   
   class << self
     def run(start=0)
@@ -14,33 +14,45 @@ class Dasher
   end
   
   def run(start)
-    Dir.chdir(SEGMENT_DIR)
-    available_segments = `ls -1tr #{VARIANTS.first}*.#{INPUT_EXTENSION}`.split.map { |s| s.gsub("#{VARIANTS.first}_", '').gsub(".#{INPUT_EXTENSION}", '') }
-    available_segments[0..-2].each { |s| mpd(s,start) } if available_segments.any?
+    while true do
+      Dir.chdir(SEGMENT_DIR)
+      index = @@last_seg + start.to_i + 1
+      available_segments = `ls -1 #{VARIANTS.first}_#{index}.#{INPUT_EXTENSION}`.split.map { |s| s.gsub("#{VARIANTS.first}_", '').gsub(".#{INPUT_EXTENSION}", '') }
+      mpd(available_segments.first,start) if available_segments.any?
+      sleep 2
+    end
   end
   
   def mpd(segment,start)
-    puts "Processing segment #{segment}"
     Dir.chdir(SEGMENT_DIR)
     available_variants = VARIANTS.select { |v| File.exists? "#{v}_#{segment}.#{INPUT_EXTENSION}" }
-    if available_variants.length == VARIANTS.length && segment.to_i >= start.to_i
+    allowed_file = File.exists? "#{VARIANTS.first}_#{segment.to_i + 1}.#{INPUT_EXTENSION}"
+    if available_variants.length == VARIANTS.length && allowed_file && segment.to_i >= start.to_i && segment.to_i > @@last_seg
+      puts "\e[31mProcessing segment #{segment}\e[0m"
       available_variants.each do |v|
         Dir.chdir(SEGMENT_DIR)
         command = "#{MP4BOX} -add \"#{v}_#{segment}.#{INPUT_EXTENSION}\" \"#{OUTPUT_DIR}/#{v}_#{segment}.mp4\"" # 
         puts "Executing: #{command}"
         `#{command}`
       end
-      files = available_variants.map { |v| "#{OUTPUT_DIR}/#{v}_#{segment}.mp4:id=#{v}" }
+      input_files = available_variants.map { |v| "#{SEGMENT_DIR}/#{v}_#{segment}.#{INPUT_EXTENSION}" }
+      raw_files = available_variants.map { |v| "#{OUTPUT_DIR}/#{v}_#{segment}.mp4" }
+      representation_files = available_variants.each_with_index.map { |v, i| "#{raw_files[i]}:id=#{v}" }
       #files = ["#{OUTPUT_DIR}/#{segment}.mp4"]
       #if File.exists?(files.first)
         Dir.chdir("#{OUTPUT_DIR}/final")
-        command = "#{MP4BOX} -dash-ctx dash-live.txt -dash 10000 -url-template -time-shift -1 -segment-name 'live_$RepresentationID$_' -out live -profile live -dynamic #{files.join(' ')}"
+        command = "#{MP4BOX} -dash-ctx dash-live.txt -dash 10000 -url-template -time-shift -1 -segment-name 'live_$RepresentationID$_' -out live -dynamic #{representation_files.join(' ')}"
         puts "Executing DASH: #{command}"
         `#{command}`
-        `read -n 1 -s`
+        `mv #{input_files.join(' ')} #{OUTPUT_DIR}/archived`
+        `mv #{raw_files.join(' ')} #{OUTPUT_DIR}/archived`
+        @@last_seg = segment.to_i
+        #`read -n 1 -s`
         #`sleep 10`
         #exit
         #end
+    else
+      puts "\e[31mSkipping segment #{segment}\e[0m"
     end
   end
 end
