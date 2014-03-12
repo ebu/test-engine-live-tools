662a9c70cfe3ea718cb30b98d72568a6c81e56be
Sjoerd Tieleman <sjoerd@sjoerdtieleman.nl>
2014-03-12 12:03:45 +0100

    Log job output

--- launcher/launcher.js
+++ launcher/launcher.js
@@ -3,11 +3,19 @@ var child_process = require('child_process');
 job_carousel = child_process.spawn('/home/bram/src/ebu-tools/carousel/carousel.sh')
 job_dasher = child_process.spawn('/home/bram/src/ebu-tools/dasher/runner.rb')
 
+job_carousel.stderr.setEncoding('utf8');
+job_carousel.stderr.on('data', function(data) {
+  console.log(data.toString());
+})
 job_carousel.on('close', function(code, signal) {
   console.log("Carousel job exited with code " + code + " and signal " + signal + ". Aborting.");
   job_dasher.kill(); // Send SIGTERM to other job
 })
 
+job_dasher.stderr.setEncoding('utf8');
+job_dasher.stderr.on('data', function(data) {
+  console.log(data.toString());
+})
 job_dasher.on('close', function(code, signal) {
   console.log("Dasher job exited with code " + code + " and signal " + signal + ". Aborting.");
   job_carousel.kill(); // Send SIGTERM to other job
