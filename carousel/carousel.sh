c52b52b6627c7cc42932de31129e898db4961683
Sjoerd Tieleman <sjoerd@sjoerdtieleman.nl>
2014-03-12 15:02:34 +0100

    Decrease number of presets.

--- carousel/carousel.sh
+++ carousel/carousel.sh
@@ -11,10 +11,8 @@ rm -f *.ts
 
 $DASH_VLC -I dummy -L $DASH_SOURCE_INPUT_FILE \
   --sout "#transcode{vcodec=h264,scale=1,venc=x264{repeat-headers=1},acodec=mpga,ab=256,channels=2}:standard{mux=ts,access=file,dst=-}" | \
-  ffmpeg -re -i - -analyzeduration 2147483647 -probesize 2147483647 -threads 0 -y \
-    -vb 3072k -s 1280x720 $VIDEO_PRESET_OPTS $HLS_OPTS out_hd_.m3u8 \
+  ffmpeg -re -i - -threads 0 -y \
     -vb 2048k -s 800x450  $VIDEO_PRESET_OPTS $HLS_OPTS out_hi_.m3u8 \
     -vb 1024k -s 640x360  $VIDEO_PRESET_OPTS $HLS_OPTS out_med_.m3u8 \
     -vb 512k  -s 320x180  $VIDEO_PRESET_OPTS $HLS_OPTS out_medlo_.m3u8 \
-    -vb 100k  -s 160x90   $VIDEO_PRESET_OPTS $HLS_OPTS out_lo_.m3u8 \
     -ab 128k              $AUDIO_PRESET_OPTS $HLS_OPTS out_audio_.m3u8
