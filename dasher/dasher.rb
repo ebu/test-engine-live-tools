bb56a8e9a004f86568e8ae85254af39a40078407
Sjoerd Tieleman <sjoerd@sjoerdtieleman.nl>
2014-03-12 17:16:10 +0100

    Use correct DASH input files.

--- dasher/dasher.rb
+++ dasher/dasher.rb
@@ -59,7 +59,7 @@ class Dasher
   def dash_intermediate_files(available_variants, segment)
     input_files          = available_variants.map { |v| "#{SEGMENT_DIR}/#{v}_#{segment}.#{INPUT_EXTENSION}" }
     raw_files            = available_variants.map { |v| "#{OUTPUT_DIR}/#{v}_#{segment}.mp4" }
-    representation_files = available_variants.each_with_index.map { |v, i| "#{OUTPUT_DIR}/#{raw_files[i]}:id=#{v}" }
+    representation_files = available_variants.each_with_index.map { |v, i| "#{raw_files[i]}:id=#{v}" }
     command = "#{MP4BOX} -dash-ctx #{OUTPUT_DIR}/dash-live.txt -dash 10000 -url-template -time-shift 1800 -segment-name 'live_$RepresentationID$_' -out #{OUTPUT_DIR}/live -dynamic #{representation_files.join(' ')}"
     puts "Executing DASH: #{command}"
     system command
