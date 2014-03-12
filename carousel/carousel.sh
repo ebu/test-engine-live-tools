158e66b0446fa3945353f4b028e58a7eb470a7cf
Sjoerd Tieleman <sjoerd@sjoerdtieleman.nl>
2014-03-12 16:57:15 +0100

    Use very fast preset

--- carousel/carousel.sh
+++ carousel/carousel.sh
@@ -3,7 +3,7 @@
 cd $DASH_SEGMENT_INPUT_DIR
 
 HLS_OPTS='-hls_time 10 -hls_list_size 30'
-VIDEO_PRESET_OPTS='-vcodec libx264 -vprofile baseline -level 31 -keyint_min 24 -g 24 -r 24 -an'
+VIDEO_PRESET_OPTS='-vcodec libx264 -vprofile baseline -preset veryfast -level 31 -keyint_min 24 -g 24 -r 24 -an'
 AUDIO_PRESET_OPTS='-acodec aac -strict -2 -ar 48000 -ac 2'
 
 rm -f *.m3u8
@@ -15,4 +15,4 @@ $DASH_VLC -I dummy -L $DASH_SOURCE_INPUT_FILE \
     -vb 2048k -s 800x450  $VIDEO_PRESET_OPTS $HLS_OPTS out_hi_.m3u8 \
     -vb 1024k -s 640x360  $VIDEO_PRESET_OPTS $HLS_OPTS out_med_.m3u8 \
     -vb 512k  -s 320x180  $VIDEO_PRESET_OPTS $HLS_OPTS out_medlo_.m3u8 \
-    -vn -ab 128k -f mpegts $AUDIO_PRESET_OPTS $HLS_OPTS out_audio_.m3u8
+    -ab 128k              $AUDIO_PRESET_OPTS $HLS_OPTS out_audio_.m3u8
