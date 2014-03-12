d08d4b2df79a83fcf9b4188cde8d81700159ba27
Sjoerd Tieleman <sjoerd@sjoerdtieleman.nl>
2014-03-12 16:25:05 +0100

    Don't CHDIR all the time.

--- dasher/dasher.rb
+++ dasher/dasher.rb
@@ -18,9 +18,8 @@ class Dasher
   def run(start)
     `rm #{OUTPUT_DIR}/*`
     while true do
-      Dir.chdir(SEGMENT_DIR)
       index = @@last_seg + start.to_i + 1
-      available_segments = `ls -1 #{VARIANTS.first}_#{index}.#{INPUT_EXTENSION}`.split.map { |s| s.gsub("#{VARIANTS.first}_", '').gsub(".#{INPUT_EXTENSION}", '') }
+      available_segments = `ls -1 #{SEGMENT_DIR}/#{VARIANTS.first}_#{index}.#{INPUT_EXTENSION}`.split.map { |s| s.gsub("#{VARIANTS.first}_", '').gsub(".#{INPUT_EXTENSION}", '') }
       # MPD the single segment if it matches
       mpd(available_segments.first,start) if available_segments.any?
       # sleep 1
@@ -28,11 +27,10 @@ class Dasher
   end
   
   def mpd(segment,start)
-    Dir.chdir(SEGMENT_DIR)
     # Check which variants are available
-    available_variants = VARIANTS.select { |v| File.exists? "#{v}_#{segment}.#{INPUT_EXTENSION}" }
+    available_variants = VARIANTS.select { |v| File.exists? "#{SEGMENT_DIR}/#{v}_#{segment}.#{INPUT_EXTENSION}" }
     # Is there also already a next segment? Than this one can be used as it is complete.
-    allowed_file = File.exists? "#{VARIANTS.first}_#{segment.to_i + 1}.#{INPUT_EXTENSION}"
+    allowed_file = File.exists? "#{SEGMENT_DIR}/#{VARIANTS.first}_#{segment.to_i + 1}.#{INPUT_EXTENSION}"
     if available_variants.length == VARIANTS.length && allowed_file && segment.to_i >= start.to_i && segment.to_i > @@last_seg
       puts "\e[31mProcessing segment #{segment}\e[0m"
       # Create intermediate files
@@ -48,8 +46,7 @@ class Dasher
   end
   
   def create_intermediate_file(variant, segment)
-    Dir.chdir(SEGMENT_DIR)
-    command = "#{MP4BOX} -add \"#{variant}_#{segment}.#{INPUT_EXTENSION}\" \"#{OUTPUT_DIR}/#{variant}_#{segment}.mp4\"" # 
+    command = "#{MP4BOX} -add \"#{SEGMENT_DIR}/#{variant}_#{segment}.#{INPUT_EXTENSION}\" \"#{OUTPUT_DIR}/#{variant}_#{segment}.mp4\"" # 
     puts "Executing: #{command}"
     `#{command}`
   end
@@ -57,9 +54,8 @@ class Dasher
   def dash_intermediate_files(available_variants, segment)
     input_files          = available_variants.map { |v| "#{SEGMENT_DIR}/#{v}_#{segment}.#{INPUT_EXTENSION}" }
     raw_files            = available_variants.map { |v| "#{OUTPUT_DIR}/#{v}_#{segment}.mp4" }
-    representation_files = available_variants.each_with_index.map { |v, i| "#{raw_files[i]}:id=#{v}" }
-    Dir.chdir(OUTPUT_DIR)
-    command = "#{MP4BOX} -dash-ctx ./dash-live.txt -dash 10000 -url-template -time-shift 1800 -segment-name 'live_$RepresentationID$_' -out live -dynamic #{representation_files.join(' ')}"
+    representation_files = available_variants.each_with_index.map { |v, i| "#{OUTPUT_DIR}/#{raw_files[i]}:id=#{v}" }
+    command = "#{MP4BOX} -dash-ctx ./dash-live.txt -dash 10000 -url-template -time-shift 1800 -segment-name 'live_$RepresentationID$_' -out #{OUTPUT_DIR}/live -dynamic #{representation_files.join(' ')}"
     puts "Executing DASH: #{command}"
     `#{command}`
     `rm #{input_files.join(' ')}`
