3af99c47a82309c97ee38c240a0ec1f683cf2ced
Sjoerd Tieleman <sjoerd@sjoerdtieleman.nl>
2014-03-12 11:22:00 +0100

    users = home

--- launcher/launcher.js
+++ launcher/launcher.js
@@ -1,7 +1,7 @@
 var child_process = require('child_process');
 
-job_carousel = child_process.spawn('/users/bram/src/ebu-tools/carousel/carousel.sh')
-job_dasher = child_process.spawn('/users/bram/src/ebu-tools/dasher/dasher.rb')
+job_carousel = child_process.spawn('/home/bram/src/ebu-tools/carousel/carousel.sh')
+job_dasher = child_process.spawn('/home/bram/src/ebu-tools/dasher/dasher.rb')
 
 job_carousel.on('close', function(code, signal) {
   console.log("Carousel job exited with code " + code + " and signal " + signal + ". Aborting.");
